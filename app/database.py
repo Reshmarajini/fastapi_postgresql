6b72a656131928d11249db7d5ed93d46be49bd74
Reshmarajini <rareshmaindia@gmail.com>
2026-02-11 18:08:55 +0530

    test

--- app/database.py
+++ app/database.py
@@ -15,18 +15,10 @@ SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
 
 
-<<<<<<< HEAD
 #mainbranch
 try:
     with engine.connect() as conn:
         print("Successfully connected to the database!")
 except Exception as e:
     print("Database connection failed!")
-=======
-try:
-    with engine.connect() as conn:
-        print("Successfully connected to the database!hiiiiiiiiiiiiiiiiiiiiiiiiii")
-except Exception as e:
-    print("Database connection failed!!!!!!!!!!!!!!!!!!!!")
->>>>>>> ccbaf6f6cf9ec06fccbad714dc5d663cbe5ee98d
     print(e)
