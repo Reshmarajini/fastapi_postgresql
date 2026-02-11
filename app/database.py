15f19e4af6e786828179b464e9919682c345e344
Reshmarajini <rareshmaindia@gmail.com>
2026-02-11 16:00:07 +0530

    refctor

--- app/database.py
+++ app/database.py
@@ -27,8 +27,8 @@ except Exception as e:
     print(e)
 
 # try:
-#     with engine.connect() as conn:
-#         print("✅ Successfully connected to the database!")
-# except Exception as e:
+     with engine.connect() as conn:
+"✅ Successfully connected to the database!")
+ except Exception as e:
 #     print("❌ Database connection failed!")
 #     print(e)
